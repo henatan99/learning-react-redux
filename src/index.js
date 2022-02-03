90fe6650f5a232750a89a1b5019f4477be029aa6
Henok Mekonnen <henatan99@yahoo.com>
2022-02-04 01:03:32 +0300

    Add Counter component that uses dispatch and selector and render in App component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './app/store';
-import { Provider } from 'react';
-
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
